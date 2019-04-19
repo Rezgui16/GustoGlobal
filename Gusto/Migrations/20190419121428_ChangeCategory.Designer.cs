12820524df7503bbe2924c39753f80ab13a1c915
Rezgui16 <soumimi1995@gmail.com>
2019-04-19 14:33:44 +0200

    modif model Category

--- Gusto/Migrations/20190419121428_ChangeCategory.Designer.cs
+++ Gusto/Migrations/20190419121428_ChangeCategory.Designer.cs
@@ -4,14 +4,16 @@ using GustoLib.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Gusto.Migrations
 {
     [DbContext(typeof(GustoDbContext))]
-    partial class GustoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190419121428_ChangeCategory")]
+    partial class ChangeCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
