cbd8195e4495e6c621264762f3166c5779d63ca6
jeffdread <cataldo.jf@free.fr>
2019-04-20 13:45:21 +0200

    dashboard area admin

---
+++ Gusto/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GustoLib.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gusto.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseAdminController
+    {
+        public DashboardController(GustoDbContext context) : base(context)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
