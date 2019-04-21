ad285d54c8c45c02bd34e6f1b5104e9298bbc988
Rezgui16 <soumimi1995@gmail.com>
2019-04-21 21:20:17 +0200

    Ajout Unique à Categorie et Ingredient

--- GustoLib/Models/Categorie.cs
+++ GustoLib/Models/Categorie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using GustoLib.Class.Validator;
 
 namespace GustoLib.Data
 {
@@ -13,6 +14,7 @@ namespace GustoLib.Data
         [Display(Name = "Name", Prompt = "Dessert")]
         [StringLength(50)]
         [Required]
+        [Unique]
         public string Name { get; set; }
         [Display(Name = "Description", Prompt = "petites douceurs...")]
         public string Description { get; set; }
