58d043a224d51ac67386c601230d4e026470e671
Rezgui16 <soumimi1995@gmail.com>
2019-04-25 02:53:53 +0200

    ajout search Recette, affichage categorie

--- Gusto/Controllers/HomeController.cs
+++ Gusto/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Gusto.Controllers
             using (var C = _context )
             {
 
-              
+              //expression lambda 
                 vm.ListCategorie = C.Categorie.Select(p => new Categorie { Name = p.Name, ID = p.ID }).ToList();
                 vm.RecetteFive = C.Recette.OrderByDescending(x => x.Moyenne).Take(3).ToList();
                 vm.RecetteFacile = C.Recette.Where(p => p.Difficulte == "Facile").ToList();
@@ -33,7 +33,27 @@ namespace Gusto.Controllers
             return View(vm);
             
         }
+        public IActionResult SaveSearch(string RechrecheRecette)
+        {
+            var vm = new SearchViewModel();
+            using (var C= _context)
+            {
+                vm.SearchRecetteResult = C.Recette.Where(p => p.Titre == RechrecheRecette).ToList();
+                
+            }
 
+            return View(vm);
+        }
+
+        public IActionResult CatSearch(int Categorie)
+        {
+            var vm = new SearchViewModel();
+            using (var C = _context) {
+                
+                vm.SearchRecetteResult = C.Recette.Where(p => p.CategorieID == Categorie).ToList();
+                    }
+            return View(vm);
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -44,5 +64,7 @@ namespace Gusto.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
---
+++ Gusto/Models/SearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GustoLib.Data;
+
+namespace Gusto.Models
+{
+    public class SearchViewModel
+    {   public SearchViewModel() { SearchRecetteResult = new List<Recette>(); }
+        
+
+
+             public List<Recette> SearchRecetteResult { get; set; }
+    }
+
+   
+}
