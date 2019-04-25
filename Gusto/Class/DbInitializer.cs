b11c6eb87e500448b6bfe22799b3715aecaff6df
jeffdread <cataldo.jf@free.fr>
2019-04-25 13:54:43 +0200

    utilisation du seed

---
+++ Gusto/Class/DbInitializer.cs
@@ -0,0 +1,41 @@
+﻿using GustoLib.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gusto.Class
+{
+    public static class DbInitializer
+    {
+        public static async Task InitializeAsync(GustoDbContext context, IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Chef", "User" };
+            IdentityResult roleResult;
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //var UserManager = serviceProvider.GetRequiredService <UserManager<User>>();
+            //var SignInManager = serviceProvider.GetRequiredService<SignInManager<User>>();
+            //if (!await UserManager.GetUserName(jeff@test.fr)) { }
+            //    var user = new User()
+            //{
+            //    UserName = "jeff@test.fr",
+            //    Email = "jeff@test.fr",
+            //    LastName = "jeff",
+            //    FirstMidName = "test"
+            //};
+            //await UserManager.AddToRoleAsync(user, "User");
+            //await SignInManager.SignInAsync(user, isPersistent: false);
+        }
+    }
+}
--- Gusto/Program.cs
+++ Gusto/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Gusto.Class;
+using GustoLib.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Gusto
@@ -14,11 +17,35 @@ namespace Gusto
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<GustoDbContext>();
+                    DbInitializer.InitializeAsync(context, services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Une erreur est apparue lors du chargement intial de la base");
+                }
+            }
+            host.Run();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            public static IWebHost BuildWebHost(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
     }
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateWebHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //        WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>();
+    //}
 }
